1c2566cc17643c554f1437612ffeec9a2ddfac46
Fabian Roh <fabiankmroh@gmail.com>
2018-08-22 22:36:49 +0900

    Different Sound Sensor added

--- sound_sensor/sound_sensor.ino
+++ sound_sensor/sound_sensor.ino
@@ -1,21 +1,27 @@
-int soundDetectedPin =10;
-int soundDetectedVal =HIGH;
-boolean bAlarm =false;
-unsigned long lastSoundDetectTime; int soundAlarmTime =500;
-
-void setup(){
-Serial.begin(9600); pinMode(soundDetectedPin,INPUT) ;
+// Declaration and initialization of the input pin
+int Analog_Eingang = A1;
+  
+void setup ()
+{
+  pinMode (Analog_Eingang, INPUT);
+       
+  Serial.begin (9600);
 }
+  
+// The program reads the current value of the input pins
+// and outputs it via serial out 
+void loop ()
+{
+  float Analog;
 
-void loop(){
-soundDetectedVal =digitalRead(soundDetectedPin) ; //소리 값을 읽어서 저장
-Serial.println(soundDetectedVal);
-if(soundDetectedVal == LOW) { // 소리가 들리면
-lastSoundDetectTime =millis(); // 소리가 들린시간 기록
-if(!bAlarm) { Serial.println("LOUD, LOUD"); bAlarm =true;
-}
-} else {
-if( (millis() -lastSoundDetectTime) >soundAlarmTime && bAlarm) { Serial.println("quiet");
-bAlarm =false;
-} }
+    
+  // Current value will be read and converted to the voltage
+  Analog = analogRead (Analog_Eingang); 
+    
+  // and outputted here
+  Serial.print (Analog, 4); 
+
+  
+  Serial.println ("----------------------------------------------------------------");
+  delay (200);
 }
